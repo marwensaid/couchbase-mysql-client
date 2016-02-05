7ec27980b9c2d298171cc59d404abf1629ea0267
marwensaid <marwensaidi1@gmail.com>
2016-02-05 15:22:46 +0100

    feat (PoC Client-Mysql-Couchbase) add output -MS

--- src/main/java/mysql/MySQLTestHarness.java
+++ src/main/java/mysql/MySQLTestHarness.java
@@ -1,13 +1,17 @@
 package mysql;
 
 import com.google.gson.Gson;
-import common.Configuration;
 import mysql.thread.*;
 
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.sql.Connection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.CountDownLatch;
+
+import static java.lang.System.currentTimeMillis;
 
 /**
  * Created by msaidi on 2/5/16.
@@ -46,11 +50,10 @@ public class MySQLTestHarness {
     public void run() {
         int iterations = config.getIterations();
         int messagesPerSecond = config.getReadsPerSecond() + config.getWritesPerSecond();
-        CountDownLatch countDownLatch = new CountDownLatch (iterations * messagesPerSecond);
+        CountDownLatch countDownLatch = new CountDownLatch(iterations * messagesPerSecond);
 
         Map<Integer, Long> callTimes = dispatchEvents(countDownLatch);
 
-        // Wait for all request threads to finish.
         try {
             countDownLatch.await();
         } catch (Exception ex) {
@@ -60,9 +63,25 @@ public class MySQLTestHarness {
         printResults(callTimes);
     }
 
-    private void printResults(Map<Integer, Long> callTimes) {
+    private void printResults(Map<Integer, Long> timePerCall) {
+        int time = 0;
 
+        for (Integer i : timePerCall.keySet()) {
+            time += timePerCall.get(i);
+            outputContent += timePerCall.get(i) + "\n";
+        }
+
+        outputContent += "Total Time: " + time;
+        try {
+            FileWriter fileWriter = new FileWriter(config.getOutputFileName());
+            BufferedWriter out = new BufferedWriter(fileWriter);
+            out.write(outputContent);
+            out.close();
+        } catch (Exception e) {
+            System.err.println("Error: " + e.getMessage());
+        }
     }
+
 
     private Map<Integer, Long> dispatchEvents(CountDownLatch countDownLatch) {
         ConcurrentHashMap<Integer, Long> callTimes = new ConcurrentHashMap<Integer, Long>();
@@ -75,24 +94,20 @@ public class MySQLTestHarness {
 
         try {
             for (int rt = 0; rt < iterations; rt++) {
-                long start = System.currentTimeMillis();
+                long start = currentTimeMillis();
                 if (workloadType.equals("read")) {
                     for (int i = 0; i < readsPerSecond; i++) {
                         new MySQLReadRequestThread(countDownLatch, config_map, callTimes, (rt * messagesPerSecond) + i);
                     }
-                }
-                else if (workloadType.equals("write")) {
+                } else if (workloadType.equals("write")) {
                     for (int i = 0; i < readsPerSecond; i++) {
-                        new WriteRequestThread(countDownLatch, config_map, callTimes, (rt * messagesPerSecond) + i, config.getInsertEntries() );
+                        new WriteRequestThread(countDownLatch, config_map, callTimes, (rt * messagesPerSecond) + i, config.getInsertEntries());
                     }
-                }
-                else if (workloadType.equals("game_read")) {
+                } else if (workloadType.equals("game_read")) {
                     new ReadThread(countDownLatch, config_map, callTimes, (rt * messagesPerSecond));
-                }
-                else if (workloadType.equals("game_write")) {
+                } else if (workloadType.equals("game_write")) {
                     new WriteThread(countDownLatch, config_map, callTimes, (rt * messagesPerSecond), config.getInsertEntries());
-                }
-                else {
+                } else {
                     for (int i = 0; i < readsPerSecond; i++) {
                         new ReadWriteRequestThread(countDownLatch, config_map, callTimes, (rt * messagesPerSecond) + i, config.getInsertEntries());
                     }
@@ -102,7 +117,7 @@ public class MySQLTestHarness {
                 for (int i = readsPerSecond; i < messagesPerSecond; i++) {
                     System.out.println("Implement write request thread");
                 }
-                long finish = System.currentTimeMillis();
+                long finish = currentTimeMillis();
                 long difference = finish - start;
                 outputContent += "Time to dispatch " + rt + ": " + difference + "ms\n";
                 if (difference > 1000) {
@@ -115,5 +130,10 @@ public class MySQLTestHarness {
             ex.printStackTrace();
         }
         return callTimes;
+    }
+
+    public static Connection generateConnection(String userName, String password, String serverName, String port, String databaseName) {
+
+        return null;
     }
 }
