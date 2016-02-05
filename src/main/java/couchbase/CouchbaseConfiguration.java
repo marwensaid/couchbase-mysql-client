ea69617a3e6c23e8aa211c93458130bc41cba388
marwensaid <marwensaidi1@gmail.com>
2016-02-05 09:54:06 +0100

    feat (PoC Client-Mysql-Couchbase) accessor -MS

--- src/main/java/couchbase/CouchbaseConfiguration.java
+++ src/main/java/couchbase/CouchbaseConfiguration.java
@@ -17,6 +17,54 @@ public class CouchbaseConfiguration extends Configuration {
     private String docName;
     private List<URI> uris;
 
+    public String getViewName() {
+        return viewName;
+    }
+
+    public void setViewName(String viewName) {
+        this.viewName = viewName;
+    }
+
+    public boolean isSetGroup() {
+        return setGroup;
+    }
+
+    public void setSetGroup(boolean setGroup) {
+        this.setGroup = setGroup;
+    }
+
+    public int getGroupLevel() {
+        return groupLevel;
+    }
+
+    public void setGroupLevel(int groupLevel) {
+        this.groupLevel = groupLevel;
+    }
+
+    public boolean isSetReduce() {
+        return setReduce;
+    }
+
+    public void setSetReduce(boolean setReduce) {
+        this.setReduce = setReduce;
+    }
+
+    public String getDocName() {
+        return docName;
+    }
+
+    public void setDocName(String docName) {
+        this.docName = docName;
+    }
+
+    public List<URI> getUris() {
+        return uris;
+    }
+
+    public void setUris(List<URI> uris) {
+        this.uris = uris;
+    }
+
     public ConcurrentHashMap<String, Object> generateConcurrentHashMap() {
         ConcurrentHashMap<String, Object> concurrentHashMap = new ConcurrentHashMap<String, Object>(super.generateConcurrentHashMap());
         concurrentHashMap.put("viewName", viewName);
