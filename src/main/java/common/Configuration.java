63b5befa48b1c222e62c5367749e6b926f336e07
marwensaid <marwensaidi1@gmail.com>
2016-02-05 13:41:14 +0100

    feat (PoC Client-Mysql-Couchbase) impl configuration / this class impl Couchbase and Mysql -MS

--- src/main/java/common/Configuration.java
+++ src/main/java/common/Configuration.java
@@ -6,7 +6,50 @@ import java.util.concurrent.ConcurrentHashMap;
  * Created by msaidi on 2/5/16.
  */
 public class Configuration {
+    private int iterations;
+    private int readsPerSecond;
+    private int writesPerSecond;
+    private String outputFileName;
+
+    public int getIterations() {
+        return iterations;
+    }
+
+    public void setIterations(int iterations) {
+        this.iterations = iterations;
+    }
+
+    public int getReadsPerSecond() {
+        return readsPerSecond;
+    }
+
+    public void setReadsPerSecond(int readsPerSecond) {
+        this.readsPerSecond = readsPerSecond;
+    }
+
+    public int getWritesPerSecond() {
+        return writesPerSecond;
+    }
+
+    public void setWritesPerSecond(int writesPerSecond) {
+        this.writesPerSecond = writesPerSecond;
+    }
+
+    public String getOutputFileName() {
+        return outputFileName;
+    }
+
+    public void setOutputFileName(String outputFileName) {
+        this.outputFileName = outputFileName;
+    }
+
     public ConcurrentHashMap<String, Object> generateConcurrentHashMap() {
-        return 0;
+        ConcurrentHashMap<String, Object> concurrentHashMap = new ConcurrentHashMap<String, Object>();
+        concurrentHashMap.put("iterations", iterations);
+        concurrentHashMap.put("readsPerSecond", readsPerSecond);
+        concurrentHashMap.put("writesPerSecond", writesPerSecond);
+        concurrentHashMap.put("outputFileName", outputFileName);
+
+        return concurrentHashMap;
     }
 }
