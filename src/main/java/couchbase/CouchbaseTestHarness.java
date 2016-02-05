c4c638751d08bbe5c500ca5f9b16d9fbed17d8b3
marwensaid <marwensaidi1@gmail.com>
2016-02-05 10:04:02 +0100

    feat (PoC Client-Mysql-Couchbase) test harness couchbase -MS

---
+++ src/main/java/couchbase/CouchbaseTestHarness.java
@@ -0,0 +1,20 @@
+package couchbase;
+
+/**
+ * Created by msaidi on 2/5/16.
+ */
+public class CouchbaseTestHarness {
+
+    public CouchbaseTestHarness() {
+    }
+
+    public static CouchbaseTestHarness createCouchbaseTestHarness(String fileName) {
+        CouchbaseTestHarness couchbaseTestHarness = new CouchbaseTestHarness();
+        couchbaseTestHarness.init(fileName);
+        return couchbaseTestHarness;
+    }
+
+    private void init(String fileName) {
+
+    }
+}
---
+++ src/main/java/couchbase/ReadRequestThread.java
@@ -0,0 +1,7 @@
+package couchbase;
+
+/**
+ * Created by msaidi on 2/5/16.
+ */
+public class ReadRequestThread {
+}
